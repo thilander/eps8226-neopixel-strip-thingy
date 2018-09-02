edc9c134f38493927f550449d236cd43c2ba6784
Andreas Thilander <andreasthilander@gmail.com>
2018-09-03 00:18:26 +0200

    NeoPixel: setvalue of single pixels + brightness

--- huzzah_server.ino
+++ huzzah_server.ino
@@ -10,14 +10,34 @@
   #include <avr/power.h>
 #endif
 
+#define NEOPIXEL_PIN 2
+
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 
 ESP8266WebServer server(80);
-
-const int led = LED_BUILTIN;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, NEOPIXEL_PIN, NEO_GRBW + NEO_KHZ800);
 
 int brightness = 50;
+
+byte neopix_gamma[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
 
 void handleRoot() {
   server.send(200, "text/plain", "/led/single (index + either r, g, b OR w)\n/brightness (?value=0-255)");
@@ -49,12 +69,15 @@ void handleSingleLed() {
   if (wS.length() > 0) {
     int w = constrain(wS.toInt(), 0, 255);
     server.send(200, "text/plain", "setting white " + String(w));
+    strip.setPixelColor(ledIndex, strip.Color(0, 0, 0, 255));
   } else {
     int r = rS.length() == 0 ? 0 : constrain(rS.toInt(), 0, 255);
     int g = gS.length() == 0 ? 0 : constrain(gS.toInt(), 0, 255);
     int b = bS.length() == 0 ? 0 : constrain(bS.toInt(), 0, 255);
+    strip.setPixelColor(ledIndex, strip.Color(r, g, b));
     server.send(200, "text/plain", "setting rgb " + String(r) + ", " + String(g) + ", " + String(b));
   }
+  strip.show();
 }
 
 void handleBrightness() {
@@ -64,6 +87,7 @@ void handleBrightness() {
     return;
   }
   brightness = constrain(bS.toInt(), 0, 255);
+  strip.setBrightness(brightness);
   server.send(200, "text/plain", String(brightness));
 }
 
@@ -93,6 +117,10 @@ void setup(void) {
   server.begin();
   digitalWrite(LED_BUILTIN, 1);
   Serial.println("HTTP server started");
+
+  strip.setBrightness(brightness);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop(void) {
